a58197c2b1f4697304b4233f2d400933aaf01df1
CatDev <Kakedev21.ja.catedrilla21@gmail.com>
2022-11-29 22:33:32 +0800

    user

--- middleware/userauthMiddleWare.js
+++ middleware/userauthMiddleWare.js
@@ -9,6 +9,8 @@ const authenticate = asyncHandler(async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id).select("-password");
 
+    //save
+
     next();
   } catch (error) {
     res.status(401);
